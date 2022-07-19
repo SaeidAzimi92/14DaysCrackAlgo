103a6c219d157b84b3058610b6604b7ba62d3fe8
saeidazimi92 <azimi.saeid92@gmail.com>
2022-07-19 21:38:51 +0430

    refactor: clean up code

--- src/main/kotlin/day1/BinarySearch.kt
+++ src/main/kotlin/day1/BinarySearch.kt
@@ -1,23 +1,13 @@
 package day1
 
 class BinarySearch {
-    fun main(args: Array<String>) {
 
-        val inputs = intArrayOf(1, 2, 3, 7, 9, 14, 18, 25, 45, 67, 69)
-        val target = 19
-
-        val index = findIndexByBinarySearch(inputs, target)
-
-        println(index)
-    }
-
-     fun findIndexByBinarySearch(inputs: IntArray, target: Int): Int {
+    fun findIndexByBinarySearch(inputs: IntArray, target: Int): Int {
         var low = 0
         var high = inputs.size - 1
         var mid: Int
 
         while (low < high) {
-
             mid = low + ((high - low) / 2)
             when {
                 target > inputs[mid] -> low = mid + 1
